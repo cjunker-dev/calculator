518d0b41198b34d05bea18c101c1475b72618dae
Celina Junker <junkerc2@nku.edu>
2021-03-30 09:42:20 -0400

    finished calculator app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator';
+  title: string = 'calculator';
+  num1: number = 0;
+  num2: number = 0;
+  answer: number = 0;
+  submit(): void{
+    this.answer= this.num1 + this.num2;
+  }
 }
